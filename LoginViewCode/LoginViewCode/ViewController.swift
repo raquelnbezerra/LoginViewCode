149e5f5e81f8aa81363bae047a5bba0cda7f8248
Raquel Quirino <raquelnbezerra@gmail.com>
2023-06-07 10:33:19 -0300

    created an element label and set constraints

--- LoginViewCode/LoginViewCode/ViewController.swift
+++ LoginViewCode/LoginViewCode/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var loginLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "Login"
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
+        self.view.addSubview(self.loginLabel)
+        self.setUpConstraints()
+    }
+    
+    private func setUpConstraints(){
+        
+        NSLayoutConstraint.activate([
+        
+            self.loginLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.loginLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+        ])
     }
     
     override func viewWillAppear(_ animated: Bool) {
