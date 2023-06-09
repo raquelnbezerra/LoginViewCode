a7b9fca1b161bae95d96a45df6fa58bc8df66d86
Raquel Quirino <raquelnbezerra@gmail.com>
2023-06-09 16:49:19 -0300

    feat: created new UI element UIImageView

--- LoginViewCode/LoginViewCode/Login/Screen/LoginScreen.swift
+++ LoginViewCode/LoginViewCode/Login/Screen/LoginScreen.swift
@@ -19,10 +19,24 @@ class LoginScreen: UIView {
         return label
     }()
     
+    lazy var logoAppImageView:UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "logo")
+        image.tintColor = .green
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
     //metodo construtor
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = UIColor(red: 24/255, green: 117/255, blue: 104/255, alpha: 1.0)
         self.addSubview(self.loginLabel)
+        self.addSubview(self.logoAppImageView)
+        
+        
+        
         self.setUpConstraints()
     }
     
@@ -31,11 +45,26 @@ class LoginScreen: UIView {
     }
     
     
+    //equalTo:
+    //basear em um determinado elemento
+    
+    //constant:
+    //me afastar/ aproximar do elemento referente(equalTo)
+    
+    //equalToConstant:
+    //setando um valor fixo para um determinado elemento.
+    
     private func setUpConstraints(){
-        
         NSLayoutConstraint.activate([
+            
+            self.loginLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor,constant: 20),
             self.loginLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            self.loginLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            
+            self.logoAppImageView.topAnchor.constraint(equalTo: self.loginLabel.bottomAnchor,constant: 20),
+            self.logoAppImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 160),
+            self.logoAppImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -160),
+            self.logoAppImageView.heightAnchor.constraint(equalToConstant: 100)
+            
         ])
     }
 
--- LoginViewCode/LoginViewCode/Login/VC/ViewController.swift
+++ LoginViewCode/LoginViewCode/Login/VC/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
     }
     
     //com o override: Estou sobrescrevendo um comportamento padrão já nativo da linguagem.
